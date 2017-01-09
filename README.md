2347f99f0149981436f4fbc39b865e14337b4b89
Brian Schorn <schorn.brian@gmail.com>
2017-01-09 04:09:09 -0600

    Completed the assignment, including PRO mode

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # Weekend 1 Challenge Starter Repo
+##Author: Brian Schorn
 
-If you want, you can start your project by forking and cloning this repo.
-
-However, feel free to start from scratch,too.
-
-**REMEMBER:** Carefully read the instructions in the assignment app.
+###My App accepts 5 elements of data and adds them to a table. It takes one of those elements (The salary) and keeps a running total for all sets of data entered. Each data set also comes with a delete button that deletes that set of data from the table and removes that datasets salary value from the running total. 
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var totalSalary = 0;
+
 $(function () {
   console.log('document is ready');
 
@@ -12,22 +14,42 @@ $(function () {
     formAsArray.forEach(function (input) {
       formData[input.name] = input.value;
     });
-
+    calcSalary(formData.employeeSalary);
     appendDom(formData);
 
     clearForm();
   });
+
+  $('#myTable').on('click', '.deleteRow', function (event) {
+    console.log('delete pressed');
+
+        var tdValue = $(this).parent().parent().children(".pay").map(function (index, val) {
+            return $(this).text();
+        }).toArray();
+        calcSalary(-parseInt(tdValue));
+        console.log(tdValue);
+        $(this).closest ('tr').remove ();
+
+    event.preventDefault();
+  });
 });
 
 function appendDom(emp) {
-  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+  // var $emp = $('<div class="employee"></div>'); // create a div jQuery object
 
-  $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
-  $emp.append('<p>' + emp.employeeIdNumber + '</p>');
-
-  $('#employees').append($emp); // append our div to the DOM
+  // $('#myTable tr:last').after('<tr><td>' + emp.employeeFirstName + '</td><td>' + emp.employeeLastName + '</td><td>' + emp.employeeIdNumber + '</td><td>' + emp.employeeJobTitle + '</td><td>' + emp.employeeSalary + '</td></tr>');
+  // $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
+  // $emp.append('<p>' + emp.employeeIdNumber + ' ' + emp.employeeJobTitle + ' ' + emp.employeeSalary + '</p');
+  $('#myTable').append('<tr><td>' + emp.employeeFirstName + '</td><td>' + emp.employeeLastName + '</td><td>' + emp.employeeIdNumber + '</td><td>' + emp.employeeJobTitle + '</td><td class="pay">' + emp.employeeSalary + '</td><td><button class="deleteRow">Delete!</button></tr>');
+  // $('#employees').append($emp); // append our div to the DOM
 }
 
 function clearForm() {
   $('form').find('input[type=text]').val('');
+}
+
+function calcSalary(salary) {
+  totalSalary += parseInt(salary);
+  console.log(totalSalary);
+  $('#salaryTable td:last').text(totalSalary);
 }
